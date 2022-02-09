9c06be115d4ff6bae635092339bac8130ec9f9a8
nadker <nadhmi.kerkeni@gmail.com>
2022-02-09 15:36:33 +0100

    envoyer consol

---
+++ src/test/java/test/FirstUnitTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class FirstUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
